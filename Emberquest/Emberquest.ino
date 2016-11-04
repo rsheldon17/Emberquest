00c8c98d9eb8a6c2034fedef907f551fb587b0c9
rsheldon17 <rsheldon17@punahou.edu>
2016-11-03 15:27:37 -1000

    Added some code from Maze_Game.pde

--- Emberquest/Emberquest.ino
+++ Emberquest/Emberquest.ino
@@ -2,10 +2,15 @@
  Emberquest by Rose Sheldon
  */
 void setup() {
-  // put your setup code here, to run once:
 #include <MeggyJrSimple.h>
+int xcoord = 3;
+int ycoord = 4;
 }
 //MeggyJr_CustomColors.pde
+void DrawBlock(int x, int y)
+{
+  DrawPx(x,y, Blue)
+}
 void loop() {
   // put your main code here, to run repeatedly:
 {
@@ -53,4 +58,17 @@ void loop() {
   DrawPx(0,7,CustomColor7);
   DrawPx(7,0,CustomColor7);
   DrawPx(7,7,CustomColor7);
+  {
+    // Maze_Game.pde
+    CheckButtonsPress();
+    if (Button_Right)
+    {  
+      if (xcoord < 7)
+        xcoord = xcoord + 1; // Also xcoord++;
+    }
+    DrawPx(xcoord,ycoord,Red); // Draws Dot
+    DisplaySlate();
+    delay(150); 
+    ClearSlate();
+  }
 }
